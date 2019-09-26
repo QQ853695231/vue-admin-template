435ee231eac782c517236c7c7b61a749b9a5c8d0
QQ853695231 <853695231@qq.com>
2019-09-26 22:48:26 +0800

    滚动加载数据。

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import VCharts from 'v-charts'
 
 /**
  * If you don't want to use mock-server
@@ -25,13 +26,13 @@ import '@/permission' // permission control
  */
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-
-Vue.config.productionTip = false
+Vue.use(ElementUI, { locale });
+Vue.use(VCharts);
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
-})
+});
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="preview-container">
-    <ul class="infinite-list" v-infinite-scroll="load">
-      <li v-for="i in count" class="infinite-list-item">{{ i }}</li>
-    </ul>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <ve-histogram :data="chartData"></ve-histogram>
+      </el-col>
+      <el-col :span="12">
+        <ve-histogram :data="chartData"></ve-histogram>
+      </el-col>
+      <el-col :span="12">
+        <ve-histogram :data="chartData"></ve-histogram>
+      </el-col>
+      <el-col :span="12">
+        <ve-histogram :data="chartData"></ve-histogram>
+      </el-col>
+    </el-row>
   </div>
 
 </template>
@@ -11,7 +22,18 @@
     export default {
         data () {
             return {
-                count: 0
+                count: 0,
+                chartData: {
+                    columns: ['日期', '访问用户', '下单用户', '下单率'],
+                    rows: [
+                        { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
+                        { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
+                        { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
+                        { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
+                        { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
+                        { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+                    ]
+                }
             }
         },
         methods: {
@@ -28,6 +50,6 @@
     width: 100%;
     position: absolute;
     overflow: auto;
-
+    overflow-x: hidden;
   }
 </style>
